2b286c854db0aaa241d7e59a5791e4ef6ad52ac1
Oxana Kochueva <oxanak99@gmail.com>
2018-06-19 20:28:32 +0300

    -

--- scripts/script.js
+++ scripts/script.js
@@ -9,18 +9,26 @@ $(window).on('scroll',function(){
     var orangePosition = Math.round($(window).scrollTop() / $(window).height() * -100);
 	$('.orange').css('transform','translateX('+(orangePosition-50)+'%)');
 
+	var logo = $('.logo');
+
+	if ((window.scrollY > 1800) && !$('.logo').hasClass('logotype')) {
+	  $('.logo').addClass('logotype');
+  } else if((window.scrollY <= 1800) && $('.logo').hasClass('logotype')) {
+	  $('.logo').removeClass('logotype');
+	}
+
 });
 
-$(window).on('scroll',function () {
-    var logo = $('.logo').offset().top + $("header").outerHeight();
-
-    if ($(window).scrollTop() > 1825) {
-        //when the header reaches the top of the window change position to fixed
-        $(".logo").css("position", "relative");
-    } else {
-        //put position back to relative
-        $(".logo").css("position", "fixed");
-    }
-});
+// $(window).on('scroll',function () {
+//     var logo = $('.logo').offset().top + $("header").outerHeight();
+//
+//     if ($(window).scrollTop() > 1825) {
+//         //when the header reaches the top of the window change position to fixed
+//         $(".logo").css("position", "relative");
+//     } else {
+//         //put position back to relative
+//         $(".logo").css("position", "fixed");
+//     }
+// });
 
 });
