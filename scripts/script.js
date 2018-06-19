5df1911f56728940313f617134e676ca2dea4ff6
Oxana Kochueva <oxanak99@gmail.com>
2018-06-19 06:06:58 +0300

    чуваки двигаются почти хорошо

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,26 @@
 $(document).ready(function(){
 
-var $orange = $('.orange'),
-$blue = $('.blue'),
-$logo = $('.logo')
 
-$(window).on('scroll', function(){
-  console.log(window.scrollY);
-  if(window.scrollY > 100) {
-  	if(!$orange.hasClass('changePosition')) {
-  		$orange.show();
-    	setTimeout(function(){$orange.toggleClass('changePosition');},0);
-  	}
-  	if(!$blue.hasClass('changePosition')) {
-  		$blue.show();
- 			setTimeout(function(){$blue.toggleClass('changePosition');},0);
-  	}
-  } else if(window.scrollY <= 100) {
-  	if($orange.hasClass('changePosition')) {
-  		$orange.hide();
-  		$orange.removeClass('changePosition');
-  	}
-  	if($blue.hasClass('changePosition')) {
-  		$blue.hide();
-  		$blue.removeClass('changePosition');
-  	}
-  }
-  if ((window.scrollY > 400)) {
-	  $logo.addClass('logotype');
-	} else if((window.scrollY <= 400)) {
-	  $logo.removeClass('logotype')
-	}
+
+$(window).on('scroll',function(){
+	console.log(window.scrollY);
+		var bluePosition = Math.round($(window).scrollTop() / $(window).height() * 100);
+    $('.blue').css('transform','translateX('+(bluePosition-50)+'%)');
+    var orangePosition = Math.round($(window).scrollTop() / $(window).height() * -100);
+	$('.orange').css('transform','translateX('+(orangePosition-50)+'%)');
+
 });
+
+$(window).on('scroll',function () {
+    var logo = $('.logo').offset().top + $("header").outerHeight();
+
+    if ($(window).scrollTop() > 1825) {
+        //when the header reaches the top of the window change position to fixed
+        $(".logo").css("position", "relative");
+    } else {
+        //put position back to relative
+        $(".logo").css("position", "fixed");
+    }
+});
+
 });
